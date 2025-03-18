5b3dff91a3b3cf2c3ca7f6a2eebfdf477379cbf2
user1 <user1@DESKTOP-11NVHPA>
2025-03-18 10:15:06 +0900

    update

--- Ex_0318/src/ex2_exception/Ex1_TryCatch.java
+++ Ex_0318/src/ex2_exception/Ex1_TryCatch.java
@@ -4,6 +4,10 @@ public class Ex1_TryCatch {
 	
 	public static void main(String[] args) {
 		
+//		try-catch(예외 처리)
+//		자바에서 JVM이 실행되는 도중 예외(에러)가 발생하면
+//		그 시점에서 프로그램이 강제적으로 종료
+		
 		int result = 0;
 		
 		result = 10 / 0;
